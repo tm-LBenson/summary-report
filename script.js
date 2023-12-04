dd9dc3654526b8e393099647ae35b8dc7d2e8f09
Lewis Benson <tm.lewis.benson@gmail.com>
2023-12-04 18:55:32 -0500

    refactor to use entire spreadsheet

--- script.js
+++ script.js
@@ -1,82 +1,173 @@
 'use strict';
 
 function processData() {
-  const preworkCount = parseInt(document.getElementById('preworkCount').value);
-  const weekCount = parseInt(document.getElementById('weekCount').value);
-  const headingsInput = document.getElementById('headings').value;
-  const studentDataInput = document.getElementById('studentData').value;
-  const attendanceDataInput = document.getElementById('attendanceData').value;
+  const rawData = document.getElementById('courseData').value;
+  const rows = rawData.split('\n').map((row) => row.split('\t'));
 
-  const headings = parseData(headingsInput);
-  const studentData = parseData(studentDataInput);
-  const attendanceData = parseAttendanceData(attendanceDataInput, weekCount);
+  const weekAndPreworkHeadings = rows[3].slice(2); // Starting from C4
+  const topicHeadings = rows[4].slice(2); // Starting from C5
+  const students = extractStudentData(
+    rows.slice(5),
+    weekAndPreworkHeadings,
+    topicHeadings
+  ); // Student data starts from row 6
 
-  const summaryHtml = generateSummary(
-    headings,
-    studentData,
-    attendanceData,
-    weekCount,
-    preworkCount
-  );
-  document.getElementById('summaryPage').innerHTML = summaryHtml;
+  displayStudentList(students);
 }
 
-function parseData(inputData) {
-  return inputData.split('\t'); 
+function extractStudentData(dataRows, weekAndPreworkHeadings, topicHeadings) {
+  let students = [];
+
+  dataRows.forEach((row) => {
+    if (row.length > 2 && row[0] !== '') {
+      let coursework = [];
+      let attendance = [];
+
+      for (let i = 0; i < weekAndPreworkHeadings.length; i++) {
+        if (
+          weekAndPreworkHeadings[i].trim() === '' &&
+          topicHeadings[i].trim() === ''
+        ) {
+          continue; // Skip columns where both headings are blank
+        }
+
+        if (topicHeadings[i].toLowerCase() === 'attendance') {
+          attendance.push(row[i + 2]); // +2 to adjust for name and email columns
+        } else {
+          coursework.push({
+            type: weekAndPreworkHeadings[i],
+            topic: topicHeadings[i],
+            result: row[i + 2],
+          });
+        }
+      }
+
+      let student = {
+        name: row[0],
+        email: row[1],
+        coursework: coursework,
+        attendance: attendance,
+        notes: row[row.length - 1],
+      };
+      students.push(student);
+    }
+  });
+
+  return students;
 }
 
-function parseAttendanceData(inputData, weekCount) {
-  const rawAttendance = inputData.split('\t');
-  let structuredAttendance = [];
-  for (let i = 0; i < weekCount; i++) {
-    structuredAttendance.push(
-      rawAttendance[i] ? rawAttendance[i] : 'Data not available'
-    );
-  }
-  return structuredAttendance;
+function displayStudentList(students) {
+  const studentListDiv = document.getElementById('studentList');
+  studentListDiv.innerHTML = '';
+
+  students.forEach((student) => {
+    const button = document.createElement('button');
+    button.classList.add('btn', 'btn-secondary', 'm-2');
+    button.innerText = student.name;
+    button.onclick = () => displayStudentSummary(student);
+    studentListDiv.appendChild(button);
+  });
+}
+function displayStudentSummary(student) {
+  const summaryDiv = document.getElementById('studentSummary');
+  let totalCatchUpTime = 0;
+  let totalAttendance = 0;
+  let notesContent = ''; // Variable to hold notes content
+
+  summaryDiv.innerHTML = `
+        <div class="container">
+            <h2 class="mt-4">${student.name}</h2>
+            <p>Email: ${student.email}</p>
+            <div class="row">`;
+
+  student.coursework.forEach((course, index) => {
+    if (course.type.toLowerCase() === 'notes:') {
+      // Save the notes content and do not create a card for it
+      notesContent = course.result;
+      return; // Skip the iteration for notes
+    }
+
+    if (course.result.trim() === '') {
+      // If there's no result, don't display this course
+      return;
+    }
+
+    const attendanceData = student.attendance[index] || '0 of 0';
+    const [attended, totalDays] = attendanceData.split(' of ').map(Number);
+    totalAttendance += attended; // Add attended days to total
+
+    let catchUpTime = 0;
+    if (
+      course.result !== 'PASS' &&
+      course.result !== 'NOT GRADED' &&
+      course.result.trim() !== ''
+    ) {
+      let [completed, total] =
+        course.result === '0'
+          ? [0, course.type.toLowerCase().includes('pre-work') ? 1 : 6]
+          : course.result.split(' out of ').map(Number);
+      catchUpTime = estimateCatchUpTime(completed, total, course.type);
+      totalCatchUpTime += isNaN(catchUpTime) ? 0 : catchUpTime; // Add catch-up time to total
+    }
+
+    // Create cards for each week's coursework and attendance
+
+    summaryDiv.innerHTML += `
+                <div class="col-md-6 mb-3">
+                    <div class="card">
+                        <div class="card-header">${course.type} (${
+      course.topic
+    })</div>
+                        <div class="card-body">
+                            <p class="card-text">Result: ${course.result}</p>
+                            <p class="card-text">Attendance: ${attendanceData}</p>
+                            ${
+                              catchUpTime
+                                ? `<p class="card-text">Estimated catch-up time: ${catchUpTime} minutes</p>`
+                                : ''
+                            }
+                        </div>
+                    </div>
+                </div>`;
+  });
+
+  // After processing coursework, create one card for Notes and Summary
+  const catchUpTimeString = totalCatchUpTime
+    ? `${totalCatchUpTime} minutes`
+    : 'N/A';
+  const attendanceString = `${totalAttendance} days`;
+
+  summaryDiv.innerHTML += `
+    <div class="col-md-12 mb-3">
+      <div class="card">
+        <div class="card-header">Notes and Summary</div>
+        <div class="card-body">
+          <p class="card-text">${notesContent}</p>
+          <p class="card-text">Total Estimated Catch-Up Time: ${catchUpTimeString}</p>
+          <p class="card-text">Total Attendance: ${attendanceString}</p>
+        </div>
+      </div>
+    </div>
+  </div>`;
+
+  summaryDiv.style.display = 'block';
 }
 
-function generateSummary(
-  headings,
-  studentData,
-  attendanceData,
-  weekCount,
-  preworkCount
-) {
-  let summary = `<h2>Summary for ${studentData[0]}</h2>`;
-  summary += `<p>Email: ${studentData[1]}</p>`;
-  summary += `<h3>Prework Assignments</h3><ul>`;
+function estimateCatchUpTime(completed, total, type) {
+  const assignmentsLeft = total - completed;
+  if (assignmentsLeft === 0) return 'N/A'; // No catch-up time needed if there are no assignments left
 
-  for (let i = 2; i < 2 + preworkCount; i++) {
-    summary += `<li>${headings[i]}: ${formatData(studentData[i])}</li>`;
+  // For pre-work, if no assignments are completed, assume 1 assignment with 60 minutes
+  if (type.toLowerCase().includes('pre-work') && completed === 0) {
+    return 60;
   }
-  summary += '</ul>';
 
-  summary += '<h3>Weekly Assignments</h3><ul>';
-  for (let i = 2 + preworkCount; i < 2 + preworkCount + weekCount; i++) {
-    summary += `<li>${headings[i]}: ${formatData(studentData[i])}</li>`;
-  }
-  summary += '</ul>';
-
-  summary += `<h3>Attendance</h3><ul>`;
-  for (let i = 0; i < weekCount; i++) {
-    summary += `<li>Week ${i + 1}: ${attendanceData[i]}</li>`;
-  }
-  summary += '</ul>';
-
-  return summary;
+  // For regular assignments, calculate the total time based on 45 minutes each
+  const timePerAssignment = 45; // Average time per assignment
+  return assignmentsLeft * timePerAssignment; // Total catch up time
 }
 
-function formatData(data) {
-  if (data === '0') {
-    return 'No submissions';
-  } else if (data === 'PASS') {
-    return 'All assignments completed';
-  } else if (data === 'NOT GRADED') {
-    return 'Grade pending';
-  } else {
-    return `${data} assignments completed`;
-  }
-}
-
-
+// Add the event listener to the process button
+document
+  .getElementById('processDataButton')
+  .addEventListener('click', processData);
