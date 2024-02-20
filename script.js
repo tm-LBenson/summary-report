c651dda3869c368a2f933ecc72f47fd947861303
Lewis Benson <tm.lewis.benson@gmail.com>
2024-02-20 08:46:39 -0500

    move logic into modular files

--- script.js
+++ script.js
@@ -1,32 +1,7 @@
 'use strict';
 
 /**  script.js Main file for the modules  */
-import { addClass, createClassInputField } from './JS/classManagement.js';
-import { processData } from './JS/dataProcessing.js';
-import { retrieveData } from './JS/retrieveData.js';
-import { toggleStudentList } from './JS/uiManagement.js';
+
 import { wakeUpBackend } from './JS/wakeUpBackend.js';
 
 wakeUpBackend();
-
-document
-  .getElementById('processDataButton')
-  .addEventListener('click', processData);
-
-document
-  .getElementById('toggleStudentsButton')
-  .addEventListener('click', toggleStudentList);
-
-document.getElementById('addClassButton').addEventListener('click', () => {
-  if (!document.getElementById('newClassName')) {
-    createClassInputField();
-  } else {
-    const container = document.getElementById('classInputContainer');
-    const newClassName = document.getElementById('newClassName').value;
-    if (newClassName) {
-      addClass(newClassName, false);
-    }
-    container.remove();
-  }
-});
-retrieveData();
