f1892ee07a17b7a0968e4c422a356fd126e57150
Lewis Benson <tm.lewis.benson@gmail.com>
2024-02-22 11:06:13 -0500

    create page layout for header component

--- src/app/instructor-portal/page.js
+++ src/app/instructor-portal/page.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import Header from '@/components/Header';
+import React from 'react';
 
 export default function page() {
   return (
-    <div>page</div>
-  )
+    <>
+      <Header
+        welcomeMessage="Instructor Portal"
+        description="Please use the specific spreadsheet template provided for course data. Ensure no stray characters or notes are included outside of the standard data cells. Any deviations from the template format may result in errors during data processing."
+      />
+      <main className="flex justify-center gap-20"></main>
+    </>
+  );
 }
