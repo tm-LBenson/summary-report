e650c4f329f0f328ca40895ed7635cc2f2cf671a
Lewis Benson <tm.lewis.benson@gmail.com>
2024-02-22 08:47:42 -0500

    Create initial nextJS app remove all old code to start fresh

--- JS/wakeUpBackend.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-/** wakeUpBackend.js The express server that powers this appliation will go to sleep if not used. This function will send a simple get request to the backend getting it spun up on page load.  */
-
-export async function wakeUpBackend() {
-  await fetch('https://astro-server-z1u9.onrender.com');
-}
--- script.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-/**  script.js Main file for the modules  */
-
-import { wakeUpBackend } from './JS/wakeUpBackend.js';
-
-wakeUpBackend();
