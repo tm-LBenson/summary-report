3b75cf52294e1c4fbd9a8bb0242fdd1fb8b1bf94
Lewis Benson <tm.lewis.benson@gmail.com>
2024-02-23 09:16:26 -0500

    move sidebar to left side of page

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} text-2xl p-4 mx-auto container`}>
+      <body className={`${inter.className} text-2xl px-0 mx-0 container`}>
         {children}
       </body>
     </html>
