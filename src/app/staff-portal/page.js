0b8d73b74148c7e2b8056b1f58a2d1278c85491e
Lewis Benson <tm.lewis.benson@gmail.com>
2024-02-23 09:17:25 -0500

    refactor into multiple sub components

--- src/app/staff-portal/page.js
+++ src/app/staff-portal/page.js
@@ -1,15 +1,16 @@
 'use client';
-import Header from '@/components/Header';
-import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Header from '@/components/Header';
+import Sidebar from '@/components/Sidebar';
+import MainContent from '../../components/MainContent';
 
-export default function page() {
-  const [classData, setClassData] = useState();
-  const [selectedClass, setSelectedClass] = useState();
-  const [displayedData, setDisplayedData] = useState();
-  function updateSelectedClassHandler(event) {
-    setSelectedClass(event.target.value);
-  }
+export default function Page() {
+  const [classData, setClassData] = useState([]);
+  const [selectedClass, setSelectedClass] = useState('');
+  const [displayedData, setDisplayedData] = useState(null);
+  const [selectedStudent, setSelectedStudent] = useState(null);
+
   useEffect(() => {
     async function fetchData() {
       const response = await axios.get(
@@ -21,56 +22,35 @@ export default function page() {
   }, []);
 
   useEffect(() => {
-    console.log(classData);
-    setDisplayedData(
-      classData?.filter((element) => {
-        return element.classId === selectedClass;
-      })[0]
+    const selectedData = classData.find(
+      (element) => element.classId === selectedClass
     );
-  }, [selectedClass]);
+    setDisplayedData(selectedData);
+  }, [selectedClass, classData]);
 
-  useEffect(() => {
-    console.log(displayedData);
-  }, [displayedData]);
+  const handleStudentClick = (student) => {
+    setSelectedStudent(student);
+  };
 
   return (
     <>
       <Header
         welcomeMessage="Staff Portal"
-        description="Welcome to the Staff Portal. To view the progress of students, please select a class from the dropdown menu below. You can then click on a student's name to see their detailed progress summary."
+        description="Welcome to the Staff Portal. Select a class and then a student to view details."
+        sidebar={true}
       />
-      <main className="flex items-center flex-col justify-center gap-20">
-        <select
-          onChange={updateSelectedClassHandler}
-          className="border w-44 border-slate-700 rounded mb-3"
-        >
-          <option value="">Select Class</option>
-          {classData &&
-            classData.map((element) => (
-              <option key={element._id} value={element.classId}>
-                {element.classId}
-              </option>
-            ))}
-        </select>
-        <section>
-          <ul className="grid grid-flow-col grid-cols-3 grid-rows-3 gap-6">
-            {displayedData?.students.map((student) => (
-              <li
-                className={[
-                  'p-5',
-                  'cursor-pointer',
-                  'bg-slate-400',
-                  'hover:bg-slate-700',
-                  'focus:bg-slate-700',
-                ].join(' ')}
-                key={student._id}
-              >
-                {student.name}
-              </li>
-            ))}
-          </ul>
-        </section>
-      </main>
+      <div className="flex">
+        <Sidebar
+          classData={classData}
+          displayedData={displayedData}
+          handleStudentClick={handleStudentClick}
+          setSelectedClass={setSelectedClass}
+        />
+        <MainContent
+          selectedStudent={selectedStudent}
+          displayedData={displayedData}
+        />
+      </div>
     </>
   );
 }
