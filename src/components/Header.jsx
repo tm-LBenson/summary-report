a59858c3834c9360b6ed67b1fb68f64da2b08f18
Lewis Benson <tm.lewis.benson@gmail.com>
2024-02-23 10:04:12 -0500

    setup spacing in heading for larger description

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
 function Header({ welcomeMessage, description, sidebar }) {
   return (
     <div className="flex">
-    {sidebar &&  <div className="w-64 h-96 reletive top-0" aria-hidden="true"></div>}
+      {sidebar && (
+        <div className="w-64 h-96 reletive top-0" aria-hidden="true"></div>
+      )}
       <header className="mx-auto mt-8 container flex flex-col items-center gap-8">
         <img className="max-w-36" src="CodeX-LogoBk.png" alt="Codex Logo" />
         <h1 className="text-6xl font-bold">{welcomeMessage}</h1>
-        <p>{description}</p>
+        <div className="flex">
+          <p className="w-3/4 mx-auto">{description}</p>
+        </div>
       </header>
     </div>
   );
