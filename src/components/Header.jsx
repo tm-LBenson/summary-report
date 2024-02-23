238b25f1b0cc1b9ee39b9e37a31299e53cacb658
Lewis Benson <tm.lewis.benson@gmail.com>
2024-02-23 09:17:55 -0500

    add placeholder component to push content in header for fixed bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
-function Header({ welcomeMessage, description }) {
+function Header({ welcomeMessage, description, sidebar }) {
   return (
-    <header className="mx-auto mt-8 container flex flex-col items-center gap-8">
-      <img className="max-w-36" src="CodeX-LogoBk.png" alt="Codex Logo" />
-      <h1 className="text-6xl font-bold">{welcomeMessage}</h1>
-      <p>{description}</p>
-    </header>
+    <div className="flex">
+    {sidebar &&  <div className="w-64 h-96 reletive top-0" aria-hidden="true"></div>}
+      <header className="mx-auto mt-8 container flex flex-col items-center gap-8">
+        <img className="max-w-36" src="CodeX-LogoBk.png" alt="Codex Logo" />
+        <h1 className="text-6xl font-bold">{welcomeMessage}</h1>
+        <p>{description}</p>
+      </header>
+    </div>
   );
 }
 
