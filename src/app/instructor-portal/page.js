854557294187b9a4826436086a03f8b2273bf736
Lewis Benson <tm.lewis.benson@gmail.com>
2024-02-23 10:03:16 -0500

    add fetch to backend for data

--- src/app/instructor-portal/page.js
+++ src/app/instructor-portal/page.js
@@ -1,15 +1,57 @@
 'use client';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import Header from '@/components/Header';
-import React from 'react';
+import Sidebar from '@/components/Sidebar';
+import MainContent from '../../components/MainContent';
 
-export default function page() {
+export default function Page() {
+  const [classData, setClassData] = useState([]);
+  const [selectedClass, setSelectedClass] = useState('');
+  const [displayedData, setDisplayedData] = useState(null);
+  const [selectedStudent, setSelectedStudent] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get(
+        'https://astro-server-z1u9.onrender.com/summary-sheets'
+      );
+      setClassData(response.data);
+    }
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const selectedData = classData.find(
+      (element) => element.classId === selectedClass
+    );
+    setDisplayedData(selectedData);
+  }, [selectedClass, classData]);
+
+  const handleStudentClick = (student) => {
+    setSelectedStudent(student);
+  };
+
   return (
     <>
       <Header
         welcomeMessage="Instructor Portal"
-        description="Please use the specific spreadsheet template provided for course data. Ensure no stray characters or notes are included outside of the standard data cells. Any deviations from the template format may result in errors during data processing."
+        description="The intended use of the Instructor Portal is for instructors to manage their classes and update the notes for the summary."
+        sidebar={true}
       />
-      <main className="flex justify-center gap-20"></main>
+      <div className="flex">
+        <Sidebar
+          classData={classData}
+          displayedData={displayedData}
+          handleStudentClick={handleStudentClick}
+          setSelectedClass={setSelectedClass}
+        />
+        <MainContent
+          allowEdit={true}
+          selectedStudent={selectedStudent}
+          displayedData={displayedData}
+        />
+      </div>
     </>
   );
 }
